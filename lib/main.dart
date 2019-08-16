5afb514991b1d48557f53cd8953ea9ef1ce2bc80
ak052142 <anveshkumar.kolluri@cerner.com>
2019-08-16 12:01:51 -0500

    Added stateful widget with sample dice images

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,25 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          ),
+          Expanded(
+            child: Image.asset('images/dice2.png'),
+          ),
+        ],
+      ),
+    );
   }
 }
